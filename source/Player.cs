bb66c94b9f198be2cb54a3525b26d064d27b5d89
unAlike <Ryan.Ungerleider@gmail.com>
2022-01-26 16:47:13 -0600

    Player Class

---
+++ source/Player.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public class Player
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+    private float health = 100;
+    private float speed = 10;
+
+    public override void _Ready()
+    {
+        
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+
+    public float GetHealth(){
+        return health;
+    }
+    public void SetHealth(float newHealth){
+        health = newHealth;
+    }
+
+    public float GetSpeed(){
+        return speed;
+    }
+    public void SetSpeed(float newSpeed){
+        speed = newSpeed;
+    }
+}
