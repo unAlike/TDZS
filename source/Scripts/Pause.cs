38df279f043e0499e4d46b4ed7c7dad9937428a6
Devlyn7299 <nwokeukud2011@my.uwstout.edu>
2022-02-09 15:42:00 -0600

    Death Menu
    Shows when the players health is 0

--- source/Scripts/Pause.cs
+++ source/Scripts/Pause.cs
@@ -3,19 +3,22 @@ using System;
 
 public class Pause : Node
 {
-    ColorRect PauseMenu, ControlsMenu;
+    ColorRect PauseMenu, ControlsMenu, DeathMenu;
     bool InControls = false;
     bool InPause = false;
+    bool IsDead = false;
+    Player Death = new Player();
 public override void _Ready()
     {
         PauseMenu = GetNode<ColorRect>("PauseMenu");
         ControlsMenu = GetNode<ColorRect>("Controls");
+        DeathMenu = GetNode<ColorRect>("DeathMenu");
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _Process(float delta)
   {
-      if(Input.IsActionJustPressed("ui_cancel") & InControls == false) {
+      if(Input.IsActionJustPressed("ui_cancel") & InControls == false & IsDead == false) {
           GetTree().Paused = !GetTree().Paused;
           PauseMenu.Visible = !PauseMenu.Visible;
           InPause = !InPause;
@@ -25,6 +28,12 @@ public override void _Ready()
           ControlsMenu.Visible = false;
           PauseMenu.Visible = true;
           InControls = false;
+      }
+
+      if(Death.GetHealth() == 0) {
+          GetTree().Paused = true;
+          DeathMenu.Visible = true;
+          IsDead = true;
       }
   }
 
@@ -44,12 +53,19 @@ public override void _Ready()
       GetTree().Paused = !GetTree().Paused;
       GetTree().ChangeScene("res://MainMenu.tscn");
       InPause = false;
+      Death.SetHealth(100);
   }
 
-  public void _on_Back_pressed() {
+   public void _on_Back_pressed() {
         ControlsMenu.Visible = false;
         PauseMenu.Visible = true;
         InControls = false;
-    }
+   }
+
+   public void _on_Replay_pressed() {
+       GetTree().ReloadCurrentScene();
+       Death.SetHealth(100);
+       GetTree().Paused = false;
+   }
 
 }
