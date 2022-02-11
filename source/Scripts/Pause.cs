509842a84e189b1dd01b53240f2792ab95c588b7
unAlike <Ryan.Ungerleider@gmail.com>
2022-02-11 15:51:09 -0600

    Replay btn fix

--- source/Scripts/Pause.cs
+++ source/Scripts/Pause.cs
@@ -8,8 +8,7 @@ public class Pause : Node
     bool InPause = false;
     bool IsDead = false;
     Player player;
-public override void _Ready()
-    {
+    public override void _Ready(){
         GD.Print(GetTree().CurrentScene.Name);
         var playerm =  (PlayerMovement)(GetTree().CurrentScene.GetNode<KinematicBody2D>("Player"));
         player = playerm.player;
@@ -18,74 +17,84 @@ public override void _Ready()
         DeathMenu = GetNode<ColorRect>("DeathMenu");
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-  public override void _Process(float delta)
-  {
-      if(Input.IsActionJustPressed("ui_cancel") & InControls == false & IsDead == false) {
-          GetTree().Paused = !GetTree().Paused;
-          PauseMenu.Visible = !PauseMenu.Visible;
-          InPause = !InPause;
-      }
-
-      if(Input.IsActionJustPressed("ui_cancel") & InControls == true) {
-          ControlsMenu.Visible = false;
-          PauseMenu.Visible = true;
-          InControls = false;
-      }
-      try{
-        if(player.GetHealth() == 0) {
-            GetTree().Paused = true;
-            DeathMenu.Visible = true;
-            IsDead = true;
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        if(Input.IsActionJustPressed("ui_cancel") & InControls == false & IsDead == false) {
+            GetTree().Paused = !GetTree().Paused;
+            PauseMenu.Visible = !PauseMenu.Visible;
+            InPause = !InPause;
         }
-      }catch(NullReferenceException){
 
-      }
-  }
+        if(Input.IsActionJustPressed("ui_cancel") & InControls == true) {
+            ControlsMenu.Visible = false;
+            PauseMenu.Visible = true;
+            InControls = false;
+        }
+        try{
+            if(player.GetHealth() == 0) {
+                GetTree().Paused = true;
+                DeathMenu.Visible = true;
+                IsDead = true;
+            }
+        }catch(NullReferenceException){
 
-  public void _on_Resume_pressed() {
-      GetTree().Paused = !GetTree().Paused;
-      PauseMenu.Visible = false;
-      InPause = false;
-  }
+        }
+    }
 
-  public void _on_Controls_pressed() {
-      ControlsMenu.Visible = true;
-      PauseMenu.Visible = false;
-      InControls = true;
-  }
+    public void _on_Resume_pressed() {
+        GetTree().Paused = !GetTree().Paused;
+        PauseMenu.Visible = false;
+        InPause = false;
+    }
 
-  public void _on_MainMenu_pressed() {
-      GetTree().Paused = !GetTree().Paused;
-      player.SetKills(0);
-      GetTree().ChangeScene("res://MainMenu.tscn");
-      foreach(Node n in GetTree().Root.GetChildren()){
-          GD.Print(n.Name);
-          if(n.Name == "Node2D"){
-              n.QueueFree();
-              GD.Print("Killed");
-          }
-          if(n.Name.Contains("Zombie")){
-              n.QueueFree();
-          }
-      }
+    public void _on_Controls_pressed() {
+        ControlsMenu.Visible = true;
+        PauseMenu.Visible = false;
+        InControls = true;
+    }
 
-      InPause = false;
-      player.SetHealth(player.GetMaxHealth());
-      
-      
-  }
+    public void _on_MainMenu_pressed() {
+        GetTree().Paused = !GetTree().Paused;
+        player.SetKills(0);
+        GetTree().ChangeScene("res://MainMenu.tscn");
+        foreach(Node n in GetTree().Root.GetChildren()){
+            GD.Print(n.Name);
+            if(n.Name == "Node2D"){
+                n.QueueFree();
+                GD.Print("Killed");
+            }
+            if(n.Name.Contains("Zombie")){
+                n.QueueFree();
+            }
+        }
 
-   public void _on_Back_pressed() {
+        InPause = false;
+        player.SetHealth(player.GetMaxHealth());
+        
+        
+    }
+
+    public void _on_Back_pressed() {
         ControlsMenu.Visible = false;
         PauseMenu.Visible = true;
         InControls = false;
-   }
+    }
 
-   public void _on_Replay_pressed() {
-       GetTree().ReloadCurrentScene();
-       player.SetHealth(player.GetMaxHealth());
-       GetTree().Paused = false;
-   }
+    public void _on_Replay_pressed() {
+        foreach(Node n in GetTree().Root.GetChildren()){
+                GD.Print(n.Name);
+                if(n.Name == "Node2D"){
+                    n.QueueFree();
+                    GD.Print("Killed");
+                }
+                if(n.Name.Contains("Zombie")){
+                    n.QueueFree();
+                }
+            }
+        GetTree().ReloadCurrentScene();
+        player.SetHealth(player.GetMaxHealth());
+        GetTree().Paused = false;
+    }
 
 }
